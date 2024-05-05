f5572bfe744ceecd6024ace503966fe7f458e130
karango29 <karina.arngoi.d.e.p@gmail.com>
2024-05-05 08:08:21 -0500

    fondos y elementos inciales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatTableModule} from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatButtonModule, 
+    MatFormFieldModule, MatInputModule, MatSelectModule
+    ,MatTableModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
